2dc5b9e38b96835185ee7f296ce0b094f372c23b
marwagaser <marwagaser@gmail.com>
2019-07-29 09:55:42 +0200

    changed button position in login

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'register', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: "", loadChildren: "./pages/home/home.module#HomePageModule" },
+  { path: "", loadChildren: "./tabs/tabs.module#TabsPageModule" },
+  { path: "register", loadChildren: "./pages/home/home.module#HomePageModule" },
+  { path: "login", loadChildren: "./pages/login/login.module#LoginPageModule" },
+  { path: "tabs", loadChildren: "./tabs/tabs.module#TabsPageModule" }
 ];
 @NgModule({
   imports: [
