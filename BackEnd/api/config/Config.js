5a4bd15ccd421e51a41d9a00b59c252cd2ff43e7
marwagaser <marwagaser@gmail.com>
2019-07-30 07:27:22 +0200

    db configs

--- BackEnd/api/config/Config.js
+++ BackEnd/api/config/Config.js
@@ -2,7 +2,7 @@ module.exports = {
   FRONTEND_URI: "http://localhost:8100/",
   SECRET: "32876qihsdh76@&#!742(*#HG&#28702y&##@^!()(&^#))jhscbd",
   MONGO_URI:
-    // "mongodb+srv://nadine180:%23Sherlock1996@cluster0-2gogh.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://nadine180:%23Sherlock1996@cluster0-2gogh.mongodb.net/test?retryWrites=true&w=majority"
 
-    "mongodb://localhost:27017/waterProject"
+  //"mongodb://localhost:27017/waterProject"
 };
