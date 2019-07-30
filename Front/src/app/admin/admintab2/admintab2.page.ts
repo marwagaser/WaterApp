66614c0acc1db799e4f69f3b6afb5ff8edc73771
ranaezzat <ranaezzat98@gmail.com>
2019-07-30 06:06:31 +0200

    Adding the deleting voucher and adding voucher functionality to the adminand fixing the front end of my work

--- Front/src/app/admin/admintab2/admintab2.page.ts
+++ Front/src/app/admin/admintab2/admintab2.page.ts
@@ -1,15 +1,130 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, AlertController, ToastController } from '../../../../node_modules/@ionic/angular';
+import { VoucherService } from '../../services/voucher-service';
+import { AuthService } from '../../auth.service';
+import { Observable } from '../../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-admintab2',
   templateUrl: './admintab2.page.html',
   styleUrls: ['./admintab2.page.scss'],
 })
-export class Admintab2Page implements OnInit {
+export class Admintab2Page {
 
-  constructor() { }
-
-  ngOnInit() {
+  vouchers: Observable<any>;
+  colors = [];
+  public myColor: string;
+  constructor(public navCtrl: NavController, public voucherService: VoucherService,
+              public alertCtrl: AlertController, public toastCtrl: ToastController,
+              public authService: AuthService) {
+                this.colors = [{
+                  color : this.getRandomColor(),
+                }];
+                this.loadVouchers();
+   }
+   loadVouchers() {
+     this.vouchers = this.voucherService.getVouchers();
+   }
+   removeVoucher(id) {
+    this.voucherService.deleteVoucher(id).subscribe(data => {
+      this.showToast(data.msg);
+      this.loadVouchers();
+    });
+  }
+  async addVoucher() {
+    const prompt = await this.alertCtrl.create({
+      message: 'Enter the information about the new voucher',
+      inputs: [
+        {
+          name: 'companyID',
+          placeholder: 'companyID'
+        },
+        {
+          name: 'voucherID',
+          placeholder: 'voucherID'
+        },
+        {
+          name: 'title',
+          placeholder: 'title'
+        },
+        {
+          name: 'offer',
+          placeholder: 'offer'
+        },
+        {
+          name: 'price',
+          placeholder: 'price'
+        },
+        {
+          name: 'promocode',
+          placeholder: 'promocode'
+        },
+        {
+          name: 'status',
+          placeholder: 'status'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            // tslint:disable-next-line:no-shadowed-variable
+            this.voucherService.addVoucher({companyID: data.companyID, voucherID: data.voucherID,
+              // tslint:disable-next-line:no-shadowed-variable
+              title: data.title, offer: data.offer, price: data.price,
+              // tslint:disable-next-line:no-shadowed-variable
+              promocode: data.promocode, status: data.status, color: data.color}).subscribe(data => {
+              this.showToast(data.msg);
+              this.loadVouchers();
+            });
+          }
+        }
+      ]
+    });
+    await prompt.present();
+  }
+  async presentConfirm(voucher) {
+    const alert = await this.alertCtrl.create({
+      message: 'Hello Admin are you sure you want to delete this offer?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Remove',
+          handler: () => {
+           this.removeVoucher(voucher._id);
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
+  getRandomColor() {
+      let color = '#';
+      for (let i = 0; i < 3; i++) {
+          const part = Math.round(Math.random() * 255).toString(16);
+          color += (part.length > 1) ? part : '0' + part;
+      }
+      this.myColor = color;
+  }
+
+   private async showToast(message: string) {
+     const toast = await this.toastCtrl.create({
+       // tslint:disable-next-line:object-literal-shorthand
+       message: message,
+       duration: 3000
+     });
+     await toast.present();
+   }
 }
+
+
--- Front/src/app/tab2/tab2.page.ts
+++ Front/src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NavController, AlertController, ToastController } from '@ionic/angular';
+import { VoucherService } from '../services/voucher-service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +10,119 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  vouchers: Observable<any>;
+  constructor(public navCtrl: NavController, public voucherService: VoucherService,
+              public alertCtrl: AlertController, public toastCtrl: ToastController,
+              public authService: AuthService) {
+     this.loadVouchers();
+   }
+   loadVouchers() {
+     this.vouchers = this.voucherService.getVouchers();
+   }
+   async addVoucher() {
+     const prompt = await this.alertCtrl.create({
+       message: 'Enter the information about the new voucher',
+       inputs: [
+         {
+           name: 'companyID',
+           placeholder: 'companyID'
+         },
+         {
+           name: 'voucherID',
+           placeholder: 'voucherID'
+         },
+         {
+           name: 'title',
+           placeholder: 'title'
+         },
+         {
+           name: 'offer',
+           placeholder: 'offer'
+         },
+         {
+           name: 'price',
+           placeholder: 'price'
+         },
+         {
+           name: 'promocode',
+           placeholder: 'promocode'
+         },
+         {
+           name: 'status',
+           placeholder: 'status'
+         }
+       ],
+       buttons: [
+         {
+           text: 'Cancel'
+         },
+         {
+           text: 'Save',
+           handler: data => {
+             // tslint:disable-next-line:no-shadowed-variable
+             this.voucherService.addVoucher({companyID: data.companyID, voucherID: data.voucherID,
+               // tslint:disable-next-line:no-shadowed-variable
+               title: data.title, offer: data.offer, price: data.price,
+               // tslint:disable-next-line:no-shadowed-variable
+               promocode: data.promocode, status: data.status, color: data.color}).subscribe(data => {
+               this.showToast(data.msg);
+               this.loadVouchers();
+             });
+           }
+         }
+       ]
+     });
+     await prompt.present();
+   }
+   removeVoucher(id) {
+     this.voucherService.deleteVoucher(id).subscribe(data => {
+       this.showToast(data.msg);
+       this.loadVouchers();
+     });
+   }
 
-  constructor() {}
+   postUserVoucher(voucher) {
+    this.authService.postUserVoucher(voucher).subscribe(data => {
+      this.showToast('Congratulations! You just purchased a voucher from ' + voucher.title + ' !');
+      this.updateUserPoints(voucher.price);
+      this.loadVouchers();
+    });
+   }
 
+   updateUserPoints(voucher) {
+    this.authService.updateUserPoints(voucher).subscribe(data => {
+      this.loadVouchers();
+    });
+   }
+
+   async presentConfirm(voucher) {
+    const alert = await this.alertCtrl.create({
+      message: 'Would you like to purchase this offer for ' + voucher.price + ' points?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Buy',
+          handler: () => {
+           this.postUserVoucher(voucher);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+   private async showToast(message: string) {
+     const toast = await this.toastCtrl.create({
+       // tslint:disable-next-line:object-literal-shorthand
+       message: message,
+       duration: 3000
+     });
+     await toast.present();
+   }
 }
